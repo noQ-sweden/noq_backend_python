26cdf67a5b04944f26a4a16b2f8e876e6643dcc7
elajth <johan.lith@gmail.com>
2024-01-10 21:26:04 +0000

    removing unwanted files, updating models, admin, settings

--- noq_django/backend/models.py
+++ noq_django/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Host(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=80)
+    address1 = models.CharField(max_length=80)
+    address2 = models.CharField(max_length=80)
+    count_of_available_places = models.IntegerField()
+    total_available_places = models.IntegerField()
+    
+    class Meta:
+        db_table = "hosts"
+        managed = True
+    
+    def __str__(self) -> str:
+        return f"{self.name} {self.address1}, {self.address2}"
