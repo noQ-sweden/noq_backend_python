e928ced2597e6aab8f758e9822726adbb0cc7d6f
elajth <johan.lith@gmail.com>
2024-01-12 19:02:36 +0000

    added models

--- noq_django/backend/admin.py
+++ noq_django/backend/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Host
+from .models import Host, Reservation, User, Room
 
 admin.site.register(Host)
+admin.site.register(Reservation)
+admin.site.register(User)
+admin.site.register(Room)
--- noq_django/backend/models.py
+++ noq_django/backend/models.py
@@ -1,16 +1,46 @@
 from django.db import models
 
+
 class Host(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=80)
     address1 = models.CharField(max_length=80)
     address2 = models.CharField(max_length=80)
     count_of_available_places = models.IntegerField()
     total_available_places = models.IntegerField()
-    
-    class Meta:
-        db_table = "hosts"
-        managed = True
-    
+
+#    class Meta:
+#        db_table = "hosts"
+#        managed = True
+
     def __str__(self) -> str:
         return f"{self.name} {self.address1}, {self.address2}"
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    unokod = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.unokod}"
+
+
+class Reservation(models.Model):
+    start_date = models.DateField()
+    # End date default to start_date +1 ?
+    host = models.ForeignKey(Host, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.host} {self.user_id} {self.start_date}"
+
+
+class Room(models.Model):
+    description = models.CharField(max_length=100)
+    total_places = models.IntegerField()
+    host = models.ForeignKey(Host, on_delete=models.CASCADE, blank=False)
+    # active for future use to track if room is possible to book
+    # effective_date for future use to track active status
