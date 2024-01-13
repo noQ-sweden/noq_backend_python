7967dbc8a68ad2198fc875eb3b70325ccc0d6023
Andre Ekespong <andre.ekespong@gmail.com>
2024-01-13 12:13:32 +0100

    Changed to postgres

--- noq_django/noq_django/settings.py
+++ noq_django/noq_django/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'noq_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'noq_db',
+        'USER': 'noq',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
