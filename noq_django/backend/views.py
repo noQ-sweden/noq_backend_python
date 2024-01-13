053daeb2c6dcc3f10cbeadd1e1cb033c5e257565
Andre Ekespong <andre.ekespong@gmail.com>
2024-01-13 22:47:43 +0100

    refactor

--- noq_django/backend/views.py
+++ noq_django/backend/views.py
@@ -4,23 +4,20 @@ from django.http import HttpResponse
 from . import models
 from . import forms
 
-def index (request):
+
+def index(request):
     return HttpResponse("Welcome to NoQ")
 
 
 def reservation_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = forms.ReservationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success_page')  # Redirect to a success page or another appropriate URL
+            return redirect(
+                "success_page"
+            )  # Redirect to a success page or another appropriate URL
     else:
         form = forms.ReservationForm()
-
-    return render(request, 'reservation.html', {'form': form, 'hosts': get_available_hosts()})
-
-def get_available_hosts():
-    # Implement logic to get available hosts for a given date
-    # This can include querying the database to find hosts that don't have reservations for the specified date
-    # Adjust this logic based on your requirements
-    return models.Host.objects.all()  # Example: Return all hosts for simplicity
+    myhosts = models.Host.objects.all()
+    return render(request, "reservation.html", {"form": form, "hosts": myhosts})
