62bbbe76256587b016587bc7daefab9ade932568
Andre Ekespong <andre.ekespong@gmail.com>
2024-01-15 23:19:11 +0100

    Generera och ta bort data

--- noq_django/backend/scripts/delete_all_data.py
+++ noq_django/backend/scripts/delete_all_data.py
@@ -3,7 +3,7 @@
 from  icecream import ic
 import sys
 
-from backend.models import Host, Reservation, User
+from backend.models import Host, Reservation, User, Product, ProductRule, ProductBooking
 
 def kontrollera(typ: str) -> bool:
     answer = input(f"Är du säker på att du vill ta bort alla {typ}?")
@@ -13,6 +13,11 @@ def kontrollera(typ: str) -> bool:
 
 def count():
     ic(Reservation.objects.all().count())
+    
+    ic(ProductBooking.objects.all().count())
+    ic(Product.objects.all().count())
+    ic(ProductRule.objects.all().count())
+    
     ic(Host.objects.all().count())
     ic(User.objects.all().count())
     
@@ -26,15 +31,31 @@ def run():
             ic(rsrv, "borttagen")
             rsrv.delete()
 
-    if kontrollera("härbärgen"):
-        for host in Host.objects.all():
-            ic(host, "borttagen")
-            host.delete()
+    if kontrollera("bokningar"):
+        for prd in ProductBooking.objects.all():
+            prd.delete()
+            ic(prd, "borttagen")
         
+    if kontrollera("produkter"):
+        for prd in Product.objects.all():
+            prd.delete()
+            ic(prd, "borttagen")
+
+    if kontrollera("produktregler"):
+        for prd in ProductRule.objects.all():
+            prd.delete()
+            ic(prd, "borttagen")
+
     if kontrollera("användare"):
         for user in User.objects.all():
             ic(user, "borttagen")
             user.delete()
+
+
+    if kontrollera("härbärgen"):
+        for host in Host.objects.all():
+            ic(host, "borttagen")
+            host.delete()
 
     count()
         
--- noq_django/backend/scripts/generate_data.py
+++ noq_django/backend/scripts/generate_data.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from icecream import ic
 from faker import Faker
 
-from backend.models import Host, User, Reservation, Room
+from backend.models import Host, User, Reservation, Product, ProductRule, ProductBooking
 
 
 def add_hosts(nbr: int) -> int:
@@ -18,21 +18,21 @@ def add_hosts(nbr: int) -> int:
     ]
 
     print("\n---- HOSTS ----")
-    
-    while len(Host.objects.all())<nbr:
-        city=faker.city()
-        id = random.randint(0,len(härbärge)-1)
+
+    while len(Host.objects.all()) < nbr:
+        city = faker.city()
+        id = random.randint(0, len(härbärge) - 1)
         ic(id)
         host_name = härbärge[id]
-        
+
         if Host.objects.filter(name=host_name, city=city).values():
             continue
-        
+
         host = Host(
             name=host_name,
             street=faker.street_address(),
             city=faker.city(),
-            total_available_places=random.randint(1,4),
+            total_available_places=random.randint(1, 4),
         )
 
         host.save()
@@ -41,57 +41,59 @@ def add_hosts(nbr: int) -> int:
 
     return
 
+
 def add_users(nbr: int):
     faker = Faker("sv_SE")
 
     print("\n---- USERS ----")
-    if len(User.objects.all())>=nbr:
+    if len(User.objects.all()) >= nbr:
         return
-    
-    while len(User.objects.all())<nbr:
-        
-        
+
+    while len(User.objects.all()) < nbr:
         gender = "M" if random.randint(0, 1) > 0 else "F"
-        
-        first_name:str = str(faker.first_name_female()) if gender == "M" else str(faker.first_name_male())
-        last_name:str = str(faker.last_name()),
-        
+
+        first_name: str = (
+            faker.first_name_female() if gender == "F" else faker.first_name_male()
+        )
+
+        last_name: str = faker.last_name()
+
         if User.objects.filter(first_name=first_name, last_name=last_name).values():
             continue
-        
+
         user = User(
             first_name=first_name,
-            last_name = last_name,
+            last_name=last_name,
             phone="070" + f"{random.randint(0,9)}-{random.randint(121212,909090)}",
-            email=f'{first_name}.{last_name}@hotmejl.se',
+            email=f"{first_name}.{last_name}@hotmejl.se",
             unokod=f"{random.randint(1000,9999)}",
-            gender = gender
+            gender=gender,
         )
         user.save()
 
-def add_reservations(nbr: int, days_ahead: int=3):
+
+def add_reservations(nbr: int, days_ahead: int = 3):
     faker = Faker("sv_SE")
     faker.seed_instance()
     max_exceptions = 20
     exceptions = 0
 
     print("\n---- RESERVATION ----")
-    
+
     host_1st = Host.objects.order_by("id").first()
     user_1st = User.objects.order_by("id").first()
-    
-    while len(Reservation.objects.all())<nbr and exceptions < max_exceptions :
+
+    while len(Reservation.objects.all()) < nbr and exceptions < max_exceptions:
         host_id = host_1st.id + random.randint(0, Host.objects.all().count() - 3)
         user_id = user_1st.id + random.randint(0, User.objects.all().count() - 5)
-        
+
         ic(host_id, user_id)
         reservation = Reservation(
             start_date=datetime.now() + timedelta(days=random.randint(1, days_ahead)),
-            
             host=Host.objects.get(id=host_id),
             user=User.objects.get(id=user_id),
         )
-        
+
         try:
             reservation.save()
             exceptions = 0
@@ -102,7 +104,29 @@ def add_reservations(nbr: int, days_ahead: int=3):
             print("User reservation added")
 
 
+def add_products(nbr: int = 3):
+    woman_only = ProductRule(name="women-only", description="Endast för kvinnor")
+    if not ProductRule.objects.filter(name=woman_only.name):
+        woman_only.save()
+
+    for host in Host.objects.all():
+        if not Product.objects.filter(host=host, name="rum"):
+            rum = Product.objects.create(
+                name="rum",
+                description="Rum med sovplatser",
+                total_places=random.randint(2, 6),
+                host=host,
+            )
+    woman_only = Product.objects.create(
+                name="woman-only",
+                description="Rum för kvinnor",
+                total_places=random.randint(2, 6),
+                host=host,
+                product_rule=woman_only
+            )
+
 def run():
     add_hosts(7)
+    add_products(2)
     add_users(12)
     add_reservations(40, 7)
