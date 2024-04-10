564b5777dfab83f50c793931b8f8bfb5272684a4
LucyMoth <fiandorian97@gmail.com>
2024-04-10 19:57:24 +0200

    added api for active requests, related to issue #287 in the priority backlog

--- noq_django/noq_django/api/host_api.py
+++ noq_django/noq_django/api/host_api.py
@@ -28,3 +28,10 @@ from django.shortcuts import get_object_or_404
 from datetime import date, timedelta
 
 router = Router(auth=lambda request: group_auth(request, "host")) #request defineras vid call, gruppnamnet är statiskt
+
+@router.get("/activerequests") #Visa mängden aktiva förfrågningar
+def fetchactiverequests(request):
+    host = Host.objects.get(user=request.user)
+    pending_bookings = Booking.objects.filter(product__host=host, status__Description='pending').count()
+
+    return pending_bookings
