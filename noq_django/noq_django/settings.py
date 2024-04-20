8634cc4c074468576dda513f3ccb3f6bea010890
Johan Lith <103529584+elajth@users.noreply.github.com>
2024-04-20 20:07:40 +0000

    Update settings.py

--- noq_django/noq_django/settings.py
+++ noq_django/noq_django/settings.py
@@ -42,7 +42,7 @@ CORS_ALLOW_HEADERS = [
     'Cookie',  
 ]
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "noqbackend.pythonanywhere.com"]
 
 
 # Application definition
@@ -158,6 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
