591529385df080484ce3cf08cd286f4399aaf1d4
Jani Rutanen <jani@noq.nu>
2024-07-03 10:32:54 +0200

    Removes commented tests for create and update

--- noq_django/noq_django/api/test/host_client.py
+++
@@ -1,8 +0,0 @@
-from django.test import Client
-from ..host_api import router
-
-
-class HostClient:
-
-    def delete_client(self):
-        self.user.delete()
--- noq_django/noq_django/api/test/test_update_product.py
+++ noq_django/noq_django/api/test/test_update_product.py
@@ -12,27 +12,33 @@ class TestUpdateProduct(TestCase):
     client = None
     username = "host"
     password = "hostpassword"
+
     def host_login(self):
+        # Create host user for the tests if not existing
         if User.objects.filter(username=self.username).first() == None:
             self.user = User.objects.create_user(username=self.username, password=self.password)
             group_obj, created = Group.objects.get_or_create(name="host")
             self.user.groups.add(group_obj)
 
+        # Login the host user
         self.client = Client(router)
         self.client.login(username=self.username, password=self.password)
 
     def delete_user(self):
+        # Delete the user created for the tests
         user = User.objects.get(username=self.username)
         user.delete()
 
     def delete_products(self):
+        # Delete all products created for the tests
         Product.objects.all().delete()
         Host.objects.all().delete()
         Region.objects.all().delete()
 
     def setUp(self):
-        #log in user and create product data
+        # log in host user for the tests
         self.host_login()
+        # Create products that can be used during the tests
         if Product.objects.filter(id=1).first() == None:
             region = Region.objects.create(name="Malmö")
             hostA = Host.objects.create(name="Host 1", street="", postcode="", city="Malmö", region_id=region.id)
@@ -42,7 +48,7 @@ class TestUpdateProduct(TestCase):
             productC = Product.objects.create(name="woman-only", total_places=3, host_id=hostB.id, type="woman-only")
 
 
-    def test_list_products(self):
+    def test_list_all_products(self):
         products_count = Product.objects.all().count()
         response = self.client.get("/api/host/products")
 
@@ -52,7 +58,7 @@ class TestUpdateProduct(TestCase):
         count = len(data)
         self.assertEqual(count, products_count)
 
-    def test_host_products(self):
+    def test_list_host_products(self):
         host = Host.objects.all().first()
         products_count = Product.objects.filter(host_id=host.id).count()
         url = "/api/host/hosts/" + str(host.id) + "/products"
@@ -77,68 +83,6 @@ class TestUpdateProduct(TestCase):
         self.assertEqual(product.description, data['description'])
         self.assertEqual(product.total_places, data['total_places'])
 
-    '''
-    def test_create_product(self):
-        products_count = Product.objects.all().count()
-        expected_count = products_count + 1
-
-        url = "/api/host/products/create"
-        data = {
-            'id': 0,
-            'name': "room",
-            "description": "3-bäddrum",
-            "total_places": 3,
-            "host": {
-                "region": {
-                    "id": 1,
-                    "name": "Malmö"
-                },
-                "id": 1,
-                "name": "Host 1",
-                "street": "",
-                "postcode": "",
-                "city": "Malmö"
-            },
-            "type": "room"
-        }
-        response = self.client.post(url, data, content_type='application/json')
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertEqual(Product.objects.all().count(), expected_count)
-
-    def test_updated_product(self):
-        product = Product.objects.all().first()
-
-        places = product.total_places
-
-        url = "/api/host/products/" + str(product.id) + "/update"
-        new_total_places = places * 2
-        data = {
-            'id': 0,
-            'name': "room",
-            "description": "3-bäddrum",
-            "total_places": new_total_places,
-            "host": {
-                "region": {
-                    "id": 1,
-                    "name": "Malmö"
-                },
-                "id": 1,
-                "name": "Host 1",
-                "street": "",
-                "postcode": "",
-                "city": "Malmö"
-            },
-            "type": "room"
-        }
-        response = self.client.post(url, data, content_type='application/json')
-
-        self.assertEqual(response.status_code, 200)
-
-        product = Product.objects.all().first()
-        self.assertEqual(product.total_places, new_total_places)
-    '''
     def test_delete_product(self):
         product = Product.objects.all().first()
         id = product.id
@@ -153,5 +97,6 @@ class TestUpdateProduct(TestCase):
         self.assertNotEqual(product.id, id)
 
     def tearDown(self):
+        # After the tests delete all data generated for the tests
         self.delete_user()
         self.delete_products()
