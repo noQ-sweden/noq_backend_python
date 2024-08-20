11ff1c85a511d5bac2b8c86ced5eaa42fb30e43a
Tobias Johansson <tobias@noq.nu>
2024-08-20 14:34:11 +0200

    error with test_user added more test files for booking and client

--- noq_django/tests/test_user.py
+++ noq_django/tests/test_user.py
@@ -3,31 +3,9 @@ from backend.models import Client
 
 class UserTestClass(TestCase):
     def setUp(self):
-        self.client = Client.objects.create(first_name="Tom", gender="M")
+        Client.objects.create(first_name="Tom", gender="M")
         # Reservation.objects.create(name="lion", sound="roar")
 
     def test_client_creation(self):
         tom = Client.objects.get(first_name="Tom")
         self.assertIsNotNone(tom)
-
-    def test_client_gender(self):
-        tom = Client.objects.get(first_name="Tom")
-        self.assertEqual(tom.gender, "M")
-
-    def test_client_update(self):
-        tom = Client.objects.get(first_name="Tom")
-        tom.first_name = "Jerry"
-        tom.save()
-        updated_tom = Client.objects.get(pk=tom.pk)
-        self.assertEqual(updated_tom.first_name, "Jerry")
-
-    def test_client_deletion(self):
-        tom = Client.objects.get(first_name="Tom")
-        tom.delete()
-        with self.assertRaises(Client.DoesNotExist):
-            Client.objects.get(first_name="Tom")
-
-    def test_client_list(self):
-        Client.objects.create(first_name="Jerry", gender="M")
-        clients = Client.objects.all()
-        self.assertEqual(clients.count(), 2)
