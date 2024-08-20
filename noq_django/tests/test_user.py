7401f6029e82d972f30c9387ef06da2fabfc33bd
Tobias Johansson <tobias@noq.nu>
2024-08-20 13:57:55 +0200

    Refactor user tests for Client model in Django.

--- noq_django/tests/test_user.py
+++ noq_django/tests/test_user.py
@@ -1,13 +1,33 @@
 from django.test import TestCase
 from backend.models import Client
 
-
-class old_tst__UserTestClass(TestCase):
+class UserTestClass(TestCase):
     def setUp(self):
-        Client.objects.create(first_name="Tom", gender="M")
+        self.client = Client.objects.create(first_name="Tom", gender="M")
         # Reservation.objects.create(name="lion", sound="roar")
 
-    def old_tst_animals_can_speak(self):
+    def test_client_creation(self):
         tom = Client.objects.get(first_name="Tom")
-
         self.assertIsNotNone(tom)
+
+    def test_client_gender(self):
+        tom = Client.objects.get(first_name="Tom")
+        self.assertEqual(tom.gender, "M")
+
+    def test_client_update(self):
+        tom = Client.objects.get(first_name="Tom")
+        tom.first_name = "Jerry"
+        tom.save()
+        updated_tom = Client.objects.get(pk=tom.pk)
+        self.assertEqual(updated_tom.first_name, "Jerry")
+
+    def test_client_deletion(self):
+        tom = Client.objects.get(first_name="Tom")
+        tom.delete()
+        with self.assertRaises(Client.DoesNotExist):
+            Client.objects.get(first_name="Tom")
+
+    def test_client_list(self):
+        Client.objects.create(first_name="Jerry", gender="M")
+        clients = Client.objects.all()
+        self.assertEqual(clients.count(), 2)
