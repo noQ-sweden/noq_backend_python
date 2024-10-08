0c0ff8242686b0edacfeed3fc729b0b07f60145b
Náti <92423248+natischmidt@users.noreply.github.com>
2024-10-08 19:21:02 +0200

    daily booking overview WIP

--- noq_django/backend/urls.py
+++ noq_django/backend/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('sleeping_spaces/create/', views.create_sleeping_space, name='create_sleeping_space'),
     path('sleeping_spaces/update/<int:pk>/', views.update_sleeping_space, name='update_sleeping_space'),
     path('sleeping_spaces/delete/<int:pk>/', views.delete_sleeping_space, name='delete_sleeping_space'),
+    path('bookings/daily/', views.daily_bookings_view, name='daily_bookings_view'),
 ]
