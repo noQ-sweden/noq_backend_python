e0949ae6ae3504d9a0860bbc90be93b6c4a4ece1
Jani Rutanen <jani@noq.nu>
2024-10-22 00:03:17 +0200

    Adds new migration

--- noq_django/backend/migrations/0014_alter_client_flag.py
+++ noq_django/backend/migrations/0014_alter_client_flag.py
@@ -2,10 +2,6 @@
 
 from django.db import migrations, models
 
-def set_default_flag_false(apps, schema_editor):
-    Client = apps.get_model('backend', 'Client')
-    Client.objects.all().update(flag=False)
-
 
 class Migration(migrations.Migration):
 
@@ -19,5 +15,4 @@ class Migration(migrations.Migration):
             name='flag',
             field=models.BooleanField(default=True),
         ),
-        migrations.RunPython(set_default_flag_false),
     ]
