7ba13f9b61459e8098ddb4a7ef2a67f2def44f87
allan zizinga <alla@noq.nu>
2025-03-19 20:28:42 +0100

    Removed sensitive key

--- noq_django/backend/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend'
-
-    def ready(self):
-        from . import signals
