20e955913b01b8852602fddd77090041640ba3ca
Mario Fernández <nilovinilo1@gmail.com>
2025-03-24 10:47:08 +0100

    Add VolunteerTask API implementation and related files

---
+++ noq_django/backend/models_volunteer_task.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.apps import apps
+
+
+class VolunteerTask(models.Model):
+  volunteer = models.ForeignKey(User, on_delete=models.CASCADE)
+  activity = models.ForeignKey("backend.Activity", on_delete=models.CASCADE, related_name="volunteer_tasks")
+  
+  STATUS_CHOICES = [
+    ('pendig', 'Pending'),
+    ('accepted', 'Accepted'),
+    ('rejected', 'Rejected'),
+    ('completed', 'Completed'),
+  ]
+  status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+  assigned_at = models.DateTimeField(auto_now_add=True)
+
+  def get_activity(self):
+          Activity = apps.get_model("backend", "Activity")
+          return Activity.objects.get(id=self.activity_id)
+
+
+  def __str__(self):
+    return f'{self.volunteer.username} - {self.activity.title} ({self.status})'
+  
