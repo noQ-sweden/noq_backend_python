da93dc8f88a076c3bf806e8ea21d24a32fddb2d0
Kavitha Maheswarappa Sujatha <kavitha@noq.nu>
2025-04-23 11:53:44 +0200

    get working

--- noq_django/rest_api/api/api.py
+++ noq_django/rest_api/api/api.py
@@ -4,7 +4,7 @@ from ninja import NinjaAPI
 from backend.models import (Host)
 from ninja.responses import Response
 from django.http import JsonResponse
-from rest_api.api.resource_api import public_router
+from rest_api.api.resource_api import router, public_router
 
 
 from .api_schemas import (
@@ -23,8 +23,8 @@ api.add_router("/host/", "rest_api.api.host_api.router")
 api.add_router("/caseworker/", "rest_api.api.caseworker_api.router")
 api.add_router("/volunteer", "rest_api.api.volunteer_api.router")
 api.add_router("/so_admin/", "rest_api.api.admin_api.router")
-api.add_router("/resources/", "rest_api.api.resource_api.router")
-api.add_router("/public/resources/", public_router)
+api.add_router("/resources/", router)  # Authenticated routes (e.g., POST, PATCH, DELETE)
+api.add_router("/public/resources/", public_router)  # Public GET routes for Swagger/docs
 
 # temporör testsektion
 api.add_router("/old/", "rest_api.api.old_api.router")
